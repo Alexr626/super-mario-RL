9e05faaefd5776e429add029224b746bb859e2db
Alexr626 <aromanus@gmail.com>
2024-10-19 15:49:52 -0700

    First commit

---
+++ src/DQN.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 32, 3,1)
+
