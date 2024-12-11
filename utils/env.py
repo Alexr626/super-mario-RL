58538581c1b4c4c9fa25b16f1c2b8ae20ebff2bc
Alexr626 <aromanus@gmail.com>
2024-12-11 08:55:33 -1000

    Made one share environment file + added util functions to load checkpoints

--- utils/env.py
+++ utils/env.py
@@ -1,19 +1,17 @@
-import gym
-import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, RIGHT_ONLY
+import numpy as np
 from gym import Wrapper
-from gym.wrappers import FrameStack, GrayScaleObservation, ResizeObservation
+from gym.wrappers import GrayScaleObservation, ResizeObservation, FrameStack
 from nes_py.wrappers import JoypadSpace
-from utils.config import *
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import RIGHT_ONLY
+from utils.config import ENV_NAME, DISPLAY, STAGES
 
-import gym
-from gym import Wrapper
 
 class SkipFrame(Wrapper):
     def __init__(self, env, skip):
         super().__init__(env)
         self.skip = skip
-
+    
     def step(self, action):
         total_reward = 0.0
         done = False
@@ -23,23 +21,13 @@ class SkipFrame(Wrapper):
             if done:
                 break
         return next_state, total_reward, done, trunc, info
+    
 
-
-def make_env():
+def make_env(skip=8):
     env = gym_super_mario_bros.make(ENV_NAME, render_mode='human' if DISPLAY else 'rgb_array', apply_api_compatibility=True)
     env = JoypadSpace(env, RIGHT_ONLY)
-    #env = LevelTracker(env, skip=4)
-    env = SkipFrame(env, skip=4)
-    env = GrayScaleObservation(env)
-    env = ResizeObservation(env, 84)
-    env = FrameStack(env, 4)
-    return env
-
-
-
-def apply_wrappers(env):
-    env = SkipFrame(env, skip=4) # Num of frames to apply one action to
     env = ResizeObservation(env, shape=84) # Resize frame from 240x256 to 84x84
     env = GrayScaleObservation(env)
     env = FrameStack(env, num_stack=4, lz4_compress=True) # May need to change lz4_compress to False if issues arise
+    env = SkipFrame(env=env, skip=skip) # Num of frames to apply one action to
     return env
--- utils/utils.py
+++ utils/utils.py
@@ -1,33 +1,38 @@
 import time
+import importlib
 from datetime import datetime
 import torch
 import csv
 import os
+import glob
 
 def get_current_date_time_string():
     return datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
 
-def save_model(model, filename):
-    """Save the model parameters to a file."""
-    torch.save(model.state_dict(), filename)
+def save_checkpoint(model, optimizer, filename):
+    checkpoint = {
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict()
+    }
+    torch.save(checkpoint, filename)
 
-def load_model(model, filename):
+def load_model(model, optimizer, filename):
     """Load model parameters from a file."""
     model.load_state_dict(torch.load(filename))
+    optimizer.load_state_dict(torch)
 
-def create_save_files_directories(timestamp, job_id, type):
+def create_save_files_directories(timestamp, job_id, type, config_version):
     # Create frames directory if needed
 
-    os.makedirs(f"frames/{timestamp}", exist_ok=True)
-    frames_dir = f"frames/{timestamp}/{job_id}_{type}"
+    frames_dir = f"results/frames/{type}_{timestamp}_conf{config_version}"
     os.makedirs(frames_dir, exist_ok=True)
 
     # Create episode_logs directory
-    episode_logs_dir = f"episode_logs/{timestamp}"
+    episode_logs_dir = f"results/episode_logs/{type}_{timestamp}_conf{config_version}"
     os.makedirs(episode_logs_dir, exist_ok=True)
 
     # Create episode_log.csv
-    log_filename = f'episode_logs/{timestamp}/{job_id}_{type}_episode_log.csv'
+    log_filename = f'results/episode_logs/{type}_{timestamp}_conf{config_version}/{job_id}_{type}_episode_log.csv'
     if not os.path.exists(log_filename):
         with open(log_filename, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
@@ -35,10 +40,9 @@ def create_save_files_directories(timestamp, job_id, type):
             writer.writerow(['episode', 'total_reward', 'episode duration', 'total duration', 'start_level', 'end_level'])
 
     # Set transitions.csv path
-    transitions_filename = f'episode_logs/{timestamp}/{job_id}_{type}/transitions.h5'
+    transitions_filename = f'episode_logs/{type}_{timestamp}_conf{config_version}/{job_id}_{type}/transitions.h5'
 
-    os.makedirs(f"checkpoints/{timestamp}", exist_ok=True)
-    checkpoints_dir = f"checkpoints/{timestamp}/{job_id}_{type}"
+    checkpoints_dir = f"results/checkpoints/{type}_{timestamp}_conf{config_version}"
     os.makedirs(checkpoints_dir, exist_ok=True)
 
     return frames_dir, checkpoints_dir, log_filename, transitions_filename
@@ -73,6 +77,41 @@ def transition_writer_thread(transition_queue, transitions_filename):
         if buffer:
             writer.writerows(buffer)
 
+def load_config(config_version, type):
+    if not config_version or not type:
+        raise ValueError("Both 'config_version' and 'implementation_type' must be provided.")
+
+    # Construct the module name based on the pattern
+    module_name = f"utils.config_{config_version}_{type}"
+    try:
+        # Import the configuration module dynamically
+        config_module = importlib.import_module(module_name)
+    except ImportError as e:
+        raise ImportError(f"Could not import config module '{module_name}'.") from e
+
+    return config_module
+
+def find_latest_checkpoint(checkpoint_dir):
+    checkpoint_files = [f for f in os.listdir(checkpoint_dir) if f.startswith(f'mario') and f.endswith('.pth')]
+    if not checkpoint_files:
+        print("None")
+        return None
+    # Extract episode numbers and sort
+    checkpoint_files.sort(key=lambda x: int(x.split('_')[-1].split('.pth')[0]), reverse=True)
+    print(checkpoint_files)
+    print(checkpoint_files[0])
+    return os.path.join(checkpoint_dir, checkpoint_files[0])
+
+def get_latest_episode_number(checkpoint_file):
+    base = os.path.basename(checkpoint_file)
+    episode_str = base.split('_')[-1].split('.pth')[0]
+    return int(episode_str)
+
+def find_episode_log_file(log_dir):
+    for file in os.listdir(log_dir):
+        if file.endswith('_episode_log.csv'):
+            return os.path.join(log_dir, file)
+    return None
 
 class Timer():
     def __init__(self):
